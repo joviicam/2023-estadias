0f08eed3d49fe94067c18b663b5ff2d00f28a34c
José Christian Narváez Figueroa <81733816+JoseNarvaezUT@users.noreply.github.com>
2023-05-31 11:25:18 -0600

    Primer commit
    Estructura del proyecto base

---
+++ estadias/src/modules/people/adapters/views/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{valueX}}</h1>
+  </div>
+</template>
+
+<script lang="ts" >
+import Vue from "vue";
+
+export default Vue.extend({
+    name:'IndexPersonVue',
+    data(){
+        return {
+            valueX:'Este valor esta aquí' as string
+        }
+    }
+})
+</script>
+
+<style>
+
+</style>
