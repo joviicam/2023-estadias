4b1572ee2d2491673c9ee11ada6b4cd9ded01a26
joviicam <20213tn110@utez.edu.mx>
2023-06-01 12:17:39 -0600

    commit que no se si se hizo

--- estadias/src/modules/people/entities/Person.ts
+++ estadias/src/modules/people/entities/Person.ts
@@ -1,6 +1,6 @@
 import { Entity } from '../../../kernel/types';
 
-//Esta clase es para definir el tipo de dato que va a tener la entidad Person
+//Esta clase es para definir el tipo de dato que va a tener la entidad Person BEAN 
 //Usamos el tipo Entity que definimos en el archivo types.ts
 export type Person = Entity<number> & {
     name : string;
